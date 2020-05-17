0bf75dc60cca1a8815a28cbe70f766a226d7ca0d
Ryan Hall <RyanNufc7@hotmail.com>
2020-05-17 18:59:14 +0100

    v0.313 - Code Cleanup, Refined Comments

--- RawInputClient/Registration.h
+++ RawInputClient/Registration.h
@@ -15,7 +15,7 @@ public:
 	std::string BuildRegistration(std::string translatedKey);	// Function for combining "Key presses" into complete licence plate reg from raw input
 private:
 
-	std::vector<std::string> m_vecstrBuildingReg;					// Vector for building characters of registration plate
+	std::vector<std::string>	m_vecstrBuildingReg;				// Vector for building characters of registration plate
 	std::string					m_strTempReg;						// Temporary string for storage to help with clearance
 	std::string					m_strCompletedRegPlate;				// Completed licence plate string
 	std::stringstream			m_ssRegBuilder;						// Stringstream to combine vector indexes together into a string
