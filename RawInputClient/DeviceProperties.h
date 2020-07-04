1a56d23a7b499edfbeddb23d816d7ee98fd02620
Ryan Hall <rj23hall@gmail.com>
2020-07-04 16:47:13 +0100

    v0.32 - Added dynamic speed limits and basic speed detection

--- RawInputClient/DeviceProperties.h
+++ RawInputClient/DeviceProperties.h
@@ -13,11 +13,15 @@ public:
 	~CDeviceProperties();
 
 	void ReadDeviceProperties();
+	void CalculateMaximumTravelTime();
 
 	std::string		m_strScannerAName;			// Scanner A device name
 	std::string		m_strScannerBName;			// Scanner B device name
 	std::string		m_strScannerLocation;		// Scanner Location
-	int				m_iSpeedLimit;				// Speed limit at location
-	float			m_fScannerDistance;			// Distance between scanners
+	int				m_iSpeedLimit;				// Speed limit at location				(MPH)
+	float			m_fScannerDistance;			// Distance between scanners			(Metres)
+	float			m_fMaximumTravelTime;		// Expected Travel time between scanners
 
+	int				m_iSecondsInHour = 3600;
+	float			m_fMPHToMiles = 1609.344;
 };
