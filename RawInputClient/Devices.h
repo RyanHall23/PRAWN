f9b448e180168b7ae99f00d534b5787fd3bcc115
Ryan Hall <rj23hall@gmail.com>
2020-07-12 20:48:48 +0100

    v0.63 - Reimplmented Registration Builder to be smoother and more lightweight

--- RawInputClient/Devices.h
+++ RawInputClient/Devices.h
@@ -14,6 +14,4 @@ public:
 	std::string TruncateHIDName(std::string strHidName);		// Function that shortens HID name
 
 	const unsigned short	m_sKeyDownFlag			= 1;		// For checking if the keyboard flag is "down"
-	const unsigned char		m_cReturnKeyVirtualCode	= 13;		// For checking if the used key is a Return key
-	const std::string		m_strReturnKeyMessage	= "!";		// Marks the end of the string with "return" notifier
 };
