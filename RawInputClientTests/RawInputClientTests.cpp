7497191d16c0e280a1957bab17ebec39ff757c75
Ryan Hall <rj23hall@gmail.com>
2021-08-18 14:45:10 +0100

    v0.824 - Refactored Unit Tests

--- RawInputClientTests/RawInputClientTests.cpp
+++ RawInputClientTests/RawInputClientTests.cpp
@@ -33,35 +33,37 @@ namespace RawInputClientTests
 		{
 			bool target = false;
 
-			CPersistence::DeviceProperties pDeviceSettings;
-			pDeviceSettings.m_vecstrAllDevices.push_back("devA");
-			pDeviceSettings.m_vecstrAllDevices.push_back("devB");
-			pDeviceSettings.RegisterDevice(1, "devA");	// Selected index from UI of alldevices vector, and navigation device name passed through, can't add device names that are navigation devices
+			CPersistence::DeviceProperties *pDeviceSettings = new CPersistence::DeviceProperties();
+			pDeviceSettings->m_vecstrAllDevices.push_back("devA");
+			pDeviceSettings->m_vecstrAllDevices.push_back("devB");
+			pDeviceSettings->RegisterDevice(1, "devA");	// Selected index from UI of alldevices vector, and navigation device name passed through, can't add device names that are navigation devices
 
-			if (std::find(pDeviceSettings.m_vecstrRegisteredDevices.begin(), pDeviceSettings.m_vecstrRegisteredDevices.end(), "devB") != pDeviceSettings.m_vecstrRegisteredDevices.end())
+			if (std::find(pDeviceSettings->m_vecstrRegisteredDevices.begin(), pDeviceSettings->m_vecstrRegisteredDevices.end(), "devB") != pDeviceSettings->m_vecstrRegisteredDevices.end())
 			{
 				target = true;
 			}
 
+			pDeviceSettings = new CPersistence::DeviceProperties();
 			Assert::IsTrue(target);
 		}
 
 		/// <summary>
-		/// Test:The deregstering of a device, by adding a device to the vector and remove it with the function
+		/// Test: The deregstering of a device, by adding a device to the vector and remove it with the function
 		/// </summary>
 		TEST_METHOD(DeregisterDevice)
 		{
 			bool target = false;
 
-			CPersistence::DeviceProperties pDeviceSettings;
-			pDeviceSettings.m_vecstrRegisteredDevices.push_back("devA");	// Emulate a registered device being added
-			pDeviceSettings.DeregisterDevice(0);
+			CPersistence::DeviceProperties* pDeviceSettings = new CPersistence::DeviceProperties();
+			pDeviceSettings->m_vecstrRegisteredDevices.push_back("devA");	// Emulate a registered device being added
+			pDeviceSettings->DeregisterDevice(0);
 
-			if (std::find(pDeviceSettings.m_vecstrRegisteredDevices.begin(), pDeviceSettings.m_vecstrRegisteredDevices.end(), "devA") == pDeviceSettings.m_vecstrRegisteredDevices.end())
+			if (std::find(pDeviceSettings->m_vecstrRegisteredDevices.begin(), pDeviceSettings->m_vecstrRegisteredDevices.end(), "devA") == pDeviceSettings->m_vecstrRegisteredDevices.end())
 			{
 				target = true;
 			}
 
+			pDeviceSettings = new CPersistence::DeviceProperties();
 			Assert::IsTrue(target);
 		}
 
@@ -71,98 +73,101 @@ namespace RawInputClientTests
 		TEST_METHOD(SafeVehicleDetection)
 		{
 			bool target = false;
-			CInputManager pInputManager;
-			pInputManager.pDevProp.m_dbOptimumTravelTime = 0.01;
-			pInputManager.pDevProp.m_vecstrRegisteredDevices.push_back("devA");
-			pInputManager.pDevProp.m_vecstrRegisteredDevices.push_back("devB");
-			pInputManager.pDevProp.m_strScannerLocation = " A1, Newcastle upon Tyne";
-			pInputManager.pDevProp.m_strDatabaseDirectory = "..//..//DrivingOffences.accdb";
+			CInputManager* pInputManager = new CInputManager();
+			pInputManager->pDevProp.m_dbOptimumTravelTime = 0.01;
+			pInputManager->pDevProp.m_vecstrRegisteredDevices.push_back("devA");
+			pInputManager->pDevProp.m_vecstrRegisteredDevices.push_back("devB");
+			pInputManager->pDevProp.m_strScannerLocation = " A1, Newcastle upon Tyne";
+			pInputManager->pDevProp.m_strDatabaseDirectory = "..//..//DrivingOffences.accdb";
 
 			std::this_thread::sleep_for(std::chrono::milliseconds(10));
-			pInputManager.InputDetected("devA", 0, 'a');
-			pInputManager.InputDetected("devA", 0, '\n');
+			pInputManager->InputDetected("devA", 0, 'a');
+			pInputManager->InputDetected("devA", 0, '\n');
 			std::this_thread::sleep_for(std::chrono::milliseconds(1000));
-			pInputManager.InputDetected("devB", 1, 'a');
-			pInputManager.InputDetected("devB", 1, '\n');
+			pInputManager->InputDetected("devB", 1, 'a');
+			pInputManager->InputDetected("devB", 1, '\n');
 
 			// Elapsed Time == 1 sec
 
-			if (pInputManager.m_strVehicleOffence == "Safe")
+			if (pInputManager->m_strVehicleOffence == "Safe")
 			{
 				target = true;
 			}
 
+			pInputManager = new CInputManager();
 			Assert::IsTrue(target);
 		}
 
 		/// <summary>
-		/// Test: Multiple time gates (I.E. 2+ scanning devices) testing if speed limit is broken
+		/// Test: Multiple time gates (I.E. 2+ scanning devices) testing if speed limit is breached
 		/// </summary>
 		TEST_METHOD(HighSpeedMultipleTimeGates)
 		{
 			bool target = false;
 
-			CInputManager pInputManager;
-			pInputManager.pDevProp.m_dbOptimumTravelTime = 2.00;
-			pInputManager.pDevProp.m_vecstrRegisteredDevices.push_back("devA");
-			pInputManager.pDevProp.m_vecstrRegisteredDevices.push_back("devB");
-			pInputManager.pDevProp.m_vecstrRegisteredDevices.push_back("devC");
-			pInputManager.pDevProp.m_strScannerLocation = " A1, Newcastle upon Tyne";
-			pInputManager.pDevProp.m_strDatabaseDirectory = "..//..//DrivingOffences.accdb";
+			CInputManager *pInputManager = new CInputManager();
+			pInputManager->pDevProp.m_dbOptimumTravelTime = 2.00;
+			pInputManager->pDevProp.m_vecstrRegisteredDevices.push_back("devA");
+			pInputManager->pDevProp.m_vecstrRegisteredDevices.push_back("devB");
+			pInputManager->pDevProp.m_vecstrRegisteredDevices.push_back("devC");
+			pInputManager->pDevProp.m_strScannerLocation = " A1, Newcastle upon Tyne";
+			pInputManager->pDevProp.m_strDatabaseDirectory = "..//..//DrivingOffences.accdb";
 
 			std::this_thread::sleep_for(std::chrono::milliseconds(2000));	// Also checks that values aren't impacted by clocks current time and are "zeroed"
-			pInputManager.InputDetected("devA", 0, 'a');
-			pInputManager.InputDetected("devA", 0, '\n');
+			pInputManager->InputDetected("devA", 0, 'a');
+			pInputManager->InputDetected("devA", 0, '\n');
 			std::this_thread::sleep_for(std::chrono::milliseconds(1000));
-			pInputManager.InputDetected("devB", 1, 'a');
-			pInputManager.InputDetected("devB", 1, '\n');
+			pInputManager->InputDetected("devB", 1, 'a');
+			pInputManager->InputDetected("devB", 1, '\n');
 			std::this_thread::sleep_for(std::chrono::milliseconds(1000));
-			pInputManager.InputDetected("devC", 2, 'a');
-			pInputManager.InputDetected("devC", 2, '\n');
+			pInputManager->InputDetected("devC", 2, 'a');
+			pInputManager->InputDetected("devC", 2, '\n');
 
 			// Time for vehicle is ~1.00sec, anything faster (lower) than this will be speeding
 
-			if (pInputManager.m_strVehicleOffence == "Speeding")
+			if (pInputManager->m_strVehicleOffence == "Speeding")
 			{
 				target = true;
 			}
 
+			pInputManager = new CInputManager();
 			Assert::IsTrue(target);
 		}
 
 		/// <summary>
-		/// Test: Multiple time gates (I.E. 2+ scanning devices) testing if speed limit is broken in reverse
+		/// Test: Multiple time gates (I.E. 2+ scanning devices) testing if speed limit is breached in reverse
 			/// </summary>
 		TEST_METHOD(HighSpeedMultipleTimeGatesWrongOrder)
 		{
 			bool target = false;
 
-			CInputManager pInputManager;
-			pInputManager.pDevProp.m_dbOptimumTravelTime = 2.00;
-			pInputManager.pDevProp.m_vecstrRegisteredDevices.push_back("devA");
-			pInputManager.pDevProp.m_vecstrRegisteredDevices.push_back("devB");
-			pInputManager.pDevProp.m_vecstrRegisteredDevices.push_back("devC");
-			pInputManager.pDevProp.m_strScannerLocation = " A1, Newcastle upon Tyne";
-			pInputManager.pDevProp.m_strDatabaseDirectory = "..//..//DrivingOffences.accdb";
+			CInputManager* pInputManager = new CInputManager();
+			pInputManager->pDevProp.m_dbOptimumTravelTime = 2.00;
+			pInputManager->pDevProp.m_vecstrRegisteredDevices.push_back("devA");
+			pInputManager->pDevProp.m_vecstrRegisteredDevices.push_back("devB");
+			pInputManager->pDevProp.m_vecstrRegisteredDevices.push_back("devC");
+			pInputManager->pDevProp.m_strScannerLocation = " A1, Newcastle upon Tyne";
+			pInputManager->pDevProp.m_strDatabaseDirectory = "..//..//DrivingOffences.accdb";
 
 
 			std::this_thread::sleep_for(std::chrono::milliseconds(2000));	// Also checks that values aren't impacted by clocks current time and are "zeroed"
-			pInputManager.InputDetected("devC", 2, 'a');
-			pInputManager.InputDetected("devC", 2, '\n');
+			pInputManager->InputDetected("devC", 2, 'a');
+			pInputManager->InputDetected("devC", 2, '\n');
 			std::this_thread::sleep_for(std::chrono::milliseconds(1000));
-			pInputManager.InputDetected("devB", 1, 'a');
-			pInputManager.InputDetected("devB", 1, '\n');
+			pInputManager->InputDetected("devB", 1, 'a');
+			pInputManager->InputDetected("devB", 1, '\n');
 			std::this_thread::sleep_for(std::chrono::milliseconds(2000));	
-			pInputManager.InputDetected("devA", 0, 'a');
-			pInputManager.InputDetected("devA", 0, '\n');
+			pInputManager->InputDetected("devA", 0, 'a');
+			pInputManager->InputDetected("devA", 0, '\n');
 
 			// Time for vehicle is ~1.00sec, anything faster (lower) than this will be speeding
 
-			if (pInputManager.m_strVehicleOffence == "Speeding & Wrong Way")
+			if (pInputManager->m_strVehicleOffence == "Speeding & Wrong Way")
 			{
 				target = true;
 			}
 
+			pInputManager = new CInputManager();
 			Assert::IsTrue(target);
 		}
 
@@ -173,27 +178,28 @@ namespace RawInputClientTests
 		TEST_METHOD(HighSpeedDetected)
 		{
 			bool target = false;
-			CInputManager pInputManager;
-			pInputManager.pDevProp.m_dbOptimumTravelTime = 2.00;
-			pInputManager.pDevProp.m_vecstrRegisteredDevices.push_back("devA");
-			pInputManager.pDevProp.m_vecstrRegisteredDevices.push_back("devB");
-			pInputManager.pDevProp.m_strScannerLocation = " A1, Newcastle upon Tyne";
-			pInputManager.pDevProp.m_strDatabaseDirectory = "..//..//DrivingOffences.accdb";
+			CInputManager *pInputManager = new CInputManager();
+			pInputManager->pDevProp.m_dbOptimumTravelTime = 2.00;
+			pInputManager->pDevProp.m_vecstrRegisteredDevices.push_back("devA");
+			pInputManager->pDevProp.m_vecstrRegisteredDevices.push_back("devB");
+			pInputManager->pDevProp.m_strScannerLocation = " A1, Newcastle upon Tyne";
+			pInputManager->pDevProp.m_strDatabaseDirectory = "..//..//DrivingOffences.accdb";
 
 			std::this_thread::sleep_for(std::chrono::milliseconds(10));
-			pInputManager.InputDetected("devA", 0,'a');
-			pInputManager.InputDetected("devA", 0, '\n');
+			pInputManager->InputDetected("devA", 0,'a');
+			pInputManager->InputDetected("devA", 0, '\n');
 			std::this_thread::sleep_for(std::chrono::milliseconds(1000));
-			pInputManager.InputDetected("devB", 1, 'a');
-			pInputManager.InputDetected("devB", 1, '\n');
+			pInputManager->InputDetected("devB", 1, 'a');
+			pInputManager->InputDetected("devB", 1, '\n');
 
 			// Elapsed Time == 1 sec
 
-			if (pInputManager.m_strVehicleOffence == "Speeding")
+			if (pInputManager->m_strVehicleOffence == "Speeding")
 			{
 				target = true;
 			}
 
+			pInputManager = new CInputManager();
 			Assert::IsTrue(target);
 		}
 
@@ -203,27 +209,28 @@ namespace RawInputClientTests
 		TEST_METHOD(WrongWayDetected)
 		{
 			bool target = false;
-			CInputManager pInputManager;
-			pInputManager.pDevProp.m_dbOptimumTravelTime = 0.1;
-			pInputManager.pDevProp.m_vecstrRegisteredDevices.push_back("devA");
-			pInputManager.pDevProp.m_vecstrRegisteredDevices.push_back("devB");
-			pInputManager.pDevProp.m_strScannerLocation = " A1, Newcastle upon Tyne";
-			pInputManager.pDevProp.m_strDatabaseDirectory = "..//..//DrivingOffences.accdb";
+			CInputManager *pInputManager = new CInputManager();
+			pInputManager->pDevProp.m_dbOptimumTravelTime = 0.1;
+			pInputManager->pDevProp.m_vecstrRegisteredDevices.push_back("devA");
+			pInputManager->pDevProp.m_vecstrRegisteredDevices.push_back("devB");
+			pInputManager->pDevProp.m_strScannerLocation = " A1, Newcastle upon Tyne";
+			pInputManager->pDevProp.m_strDatabaseDirectory = "..//..//DrivingOffences.accdb";
 
 			std::this_thread::sleep_for(std::chrono::milliseconds(10));
-			pInputManager.InputDetected("devB", 1, 'a');
-			pInputManager.InputDetected("devB", 1, '\n');
+			pInputManager->InputDetected("devB", 1, 'a');
+			pInputManager->InputDetected("devB", 1, '\n');
 			std::this_thread::sleep_for(std::chrono::milliseconds(1000));
-			pInputManager.InputDetected("devA", 0, 'a');
-			pInputManager.InputDetected("devA", 0, '\n');
+			pInputManager->InputDetected("devA", 0, 'a');
+			pInputManager->InputDetected("devA", 0, '\n');
 
 			// Elapsed Time == 1 sec
 
-			if (pInputManager.m_strVehicleOffence == "Wrong Way")
+			if (pInputManager->m_strVehicleOffence == "Wrong Way")
 			{
 				target = true;
 			}
 
+			pInputManager = new CInputManager();
 			Assert::IsTrue(target);
 		}
 
@@ -233,27 +240,28 @@ namespace RawInputClientTests
 		TEST_METHOD(WrongWayHighSpeedDetected)
 		{
 			bool target = false;
-			CInputManager pInputManager;
-			pInputManager.pDevProp.m_dbOptimumTravelTime = 2.00;
-			pInputManager.pDevProp.m_vecstrRegisteredDevices.push_back("devA");
-			pInputManager.pDevProp.m_vecstrRegisteredDevices.push_back("devB");
-			pInputManager.pDevProp.m_strScannerLocation = " A1, Newcastle upon Tyne";
-			pInputManager.pDevProp.m_strDatabaseDirectory = "..//..//DrivingOffences.accdb";
+			CInputManager *pInputManager = new CInputManager();
+			pInputManager->pDevProp.m_dbOptimumTravelTime = 2.00;
+			pInputManager->pDevProp.m_vecstrRegisteredDevices.push_back("devA");
+			pInputManager->pDevProp.m_vecstrRegisteredDevices.push_back("devB");
+			pInputManager->pDevProp.m_strScannerLocation = " A1, Newcastle upon Tyne";
+			pInputManager->pDevProp.m_strDatabaseDirectory = "..//..//DrivingOffences.accdb";
 
 			std::this_thread::sleep_for(std::chrono::milliseconds(10));
-			pInputManager.InputDetected("devB", 1, 'a');
-			pInputManager.InputDetected("devB", 1, '\n');
+			pInputManager->InputDetected("devB", 1, 'a');
+			pInputManager->InputDetected("devB", 1, '\n');
 			std::this_thread::sleep_for(std::chrono::milliseconds(1000));
-			pInputManager.InputDetected("devA", 0, 'a');
-			pInputManager.InputDetected("devA", 0, '\n');
+			pInputManager->InputDetected("devA", 0, 'a');
+			pInputManager->InputDetected("devA", 0, '\n');
 
 			// Elapsed Time == 1 sec
 
-			if (pInputManager.m_strVehicleOffence == "Speeding & Wrong Way")
+			if (pInputManager->m_strVehicleOffence == "Speeding & Wrong Way")
 			{
 				target = true;
 			}
 
+			pInputManager = new CInputManager();
 			Assert::IsTrue(target);
 		}
 	};
