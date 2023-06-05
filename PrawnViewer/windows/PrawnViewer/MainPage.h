aea4f6a509ac5bcb65e5cfb9ee2daad138a0f3bb
RyanHall23 <rj23hall@gmail.com>
2023-06-05 20:13:52 +0100

    Created React Windows project

---
+++ PrawnViewer/windows/PrawnViewer/MainPage.h
@@ -0,0 +1,19 @@
+﻿#pragma once
+#include "MainPage.g.h"
+#include <winrt/Microsoft.ReactNative.h>
+
+namespace winrt::PrawnViewer::implementation
+{
+    struct MainPage : MainPageT<MainPage>
+    {
+        MainPage();
+    };
+}
+
+namespace winrt::PrawnViewer::factory_implementation
+{
+    struct MainPage : MainPageT<MainPage, implementation::MainPage>
+    {
+    };
+}
+
