330226e128b033c46ea8a62ea337c3d5dbe2d7fc
RyanHall23 <rj23hall@gmail.com>
2024-02-20 20:48:21 +0000

    Additional test work

--- Prawn-Viewer/src/index.js
+++ Prawn-Viewer/src/index.js
@@ -9,13 +9,14 @@ if (require('electron-squirrel-startup')) {
 const createWindow = () => {
   // Create the browser window.
   const mainWindow = new BrowserWindow({
-    width: 800,
-    height: 600,
+    width: 1920,
+    height: 1000,
     webPreferences: {
       preload: path.join(__dirname, 'preload.js'),
     },
   });
 
+  mainWindow.maximize();
   // and load the index.html of the app.
   mainWindow.loadFile(path.join(__dirname, 'index.html'));
 
